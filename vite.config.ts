a204e139433a41ee33b8d6cbf6927c4427510170
felri <felipercmelo@gmail.com>
2023-02-21 07:56:39 -0300

    maybe now please

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), pluginRewriteAll()],
   server: {
       host: true,
       port: 8080
