bb880d4ddd9a8a13f01bc3df315df348f69b3462
felri <felipercmelo@gmail.com>
2023-02-23 16:47:24 -0300

    should not do this in the future

--- src/models/Background.tsx
+++ src/models/Background.tsx
@@ -1,11 +1,20 @@
+// @ts-nocheck
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { Suspense, useEffect, useRef, useState } from "react";
+// @ts-ignore
 import vertexShader from "@src/glsl/main.vert";
+// @ts-ignore
 import fragmentShader from "@src/glsl/main.frag";
 import * as THREE from "three";
 
+interface BackgroundProps {
+  resolution: [number, number];
+  mouse: [number, number];
+  time: number;
+}
+
 function Backgroung(): JSX.Element {
-  const meshRef = useRef();
+  const meshRef = useRef<THREE.Mesh>();
 
   const [mouse, setMouse] = useState([0, 0]);
   const resolution = [window.innerWidth, window.innerHeight];
@@ -14,7 +23,9 @@ function Backgroung(): JSX.Element {
     const handleResize = () => {
       resolution[0] = window.innerWidth;
       resolution[1] = window.innerHeight;
+      // @ts-ignore
       meshRef.current.material.uniforms.resolution.value = resolution;
+      // @ts-ignore
       meshRef.current.material.uniforms.resolution.needsUpdate = true;
     };
     window.addEventListener("resize", handleResize);
@@ -25,10 +36,12 @@ function Backgroung(): JSX.Element {
 
   useFrame(({ clock }) => {
     if(!meshRef.current) return;
+    // @ts-ignore
     meshRef.current.material.uniforms.time.value = clock.elapsedTime;
+    // @ts-ignore
     meshRef.current.material.uniforms.mouse.value = mouse;
   });
-
+  // @ts-ignore
   const handleMouseMove = (event) => {
     const x = event.clientX / window.innerWidth;
     const y = 1 - event.clientY / window.innerHeight;
@@ -36,6 +49,7 @@ function Backgroung(): JSX.Element {
   };
 
   return (
+    // @ts-ignore
     <mesh ref={meshRef}>
       <planeGeometry args={resolution} />
       <shaderMaterial
